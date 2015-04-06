cee4f1443caaa713e9603b7041c15bc6eb8d45d2
Fox Wilson <foxwilson123@gmail.com>
2015-04-06 13:29:38 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name="asyncio-irc",
-        version="0.1.2",
+        version="0.1.3",
         description="irc based on asyncio",
         author="Fox Wilson",
         author_email="fwilson@fwilson.me",
