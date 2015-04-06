e07c0f32a65404bb2442369376ee7ad81b6f657e
Fox Wilson <foxwilson123@gmail.com>
2015-04-05 23:58:44 -0400

    fix setup.py requires

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
         name="asyncio-irc",
-        version="0.1.1",
+        version="0.1.2",
         description="irc based on asyncio",
         author="Fox Wilson",
         author_email="fwilson@fwilson.me",
         url="https://github.com/watchtower/asyncirc",
-        requires=["blinker"],
+        install_requires=["blinker"],
         packages=["asyncirc", "asyncirc.plugins"]
 )
