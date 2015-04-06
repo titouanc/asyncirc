12e1846bb20c8bdd93d9850205da686b1d5523f3
Fox Wilson <foxwilson123@gmail.com>
2015-04-05 23:54:06 -0400

    add dependency blinker

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from distutils.core import setup
 
 setup(
         name="asyncio-irc",
-        version="0.1.0",
+        version="0.1.1",
         description="irc based on asyncio",
         author="Fox Wilson",
         author_email="fwilson@fwilson.me",
+        url="https://github.com/watchtower/asyncirc",
+        requires=["blinker"],
         packages=["asyncirc", "asyncirc.plugins"]
 )
