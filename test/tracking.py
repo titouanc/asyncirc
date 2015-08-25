58e59fa6f0ac326ff8de391ed920b284113d0f68
Fox Wilson <foxwilson123@gmail.com>
2015-08-25 14:58:40 +0000

    finish tracking tests

--- test/tracking.py
+++ test/tracking.py
@@ -16,6 +16,9 @@ def part_example_user():
 def quit_example_user():
     signal("raw").send(client, text=":example!example@example.com QUIT :Example quit reason")
 
+def kick_example_user():
+    signal("raw").send(client, text=":someop!~someop@example.org KICK #example example :Example kick reason")
+
 @test("should add users to the tracking database on channel joins")
 def test_add_objects_to_database():
     join_example_user()
@@ -47,9 +50,22 @@ def test_quit():
 def check_quit_actually_removed_user(_):
     test_quit.succeed_if("example" not in client.tracking_registry.users)
 
+test_user_return_after_quit = Test(None, "users are re-added to the registry on reconnect")
+
+@test("should remove users from channels on kick")
+def test_kick():
+    join_example_user()
+    test_user_return_after_quit.succeed_if("example" in client.tracking_registry.users and "#example" in client.tracking_registry.users["example"].channels)
+    kick_example_user()
+
+@test_kick.done.connect
+def check_kick_removes_user_from_channel(_):
+    test_kick.succeed_if("#example" not in client.tracking_registry.users["example"].channels)
+
 manager = TestManager([
     test_add_objects_to_database, test_account_recording_on_extjoin, test_host_recording,
-    test_channel_membership_join_tracking, test_channel_membership_part_tracking, test_quit
+    test_channel_membership_join_tracking, test_channel_membership_part_tracking, test_quit,
+    test_kick, test_user_return_after_quit
 ])
 
 if __name__ == '__main__':
