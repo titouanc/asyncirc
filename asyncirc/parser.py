26de3d7f1041c334d9f761719b4929bcda6c40c4
Fox Wilson <fwilson@fwilson.me>
2016-07-01 19:48:28 -0400

    parser documentation

--- asyncirc/parser.py
+++ asyncirc/parser.py
@@ -1,23 +1,33 @@
-# parser.py
-# Purpose: Conversion of RFC1459 messages to/from native objects.
-#
-# Copyright (c) 2014, William Pitcock <nenolod@dereferenced.org>
-#
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+"""
+parser.py
+Purpose: Conversion of RFC1459 messages to/from native objects.
+
+Copyright (c) 2014, William Pitcock <nenolod@dereferenced.org>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+"""
 
 class RFC1459Message(object):
+    """
+    Represents an IRC message.
+    """
+
     @classmethod
     def from_data(cls, verb, params=None, source=None, tags=None):
+        """
+        Create a new RFC1459Message from the given verb, parameters, and source
+        having the given tags.
+        """
         o = cls()
         o.verb = verb
         o.tags = dict()
@@ -37,6 +47,9 @@ class RFC1459Message(object):
 
     @classmethod
     def from_message(cls, message):
+        """
+        Create a new RFC1459Message from an unparsed IRC line.
+        """
         if isinstance(message, bytes):
             message = message.decode('UTF-8', 'replace')
 
